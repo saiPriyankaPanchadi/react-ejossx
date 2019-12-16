4e45101bc7f4a77e8fd5bfb7b94dfcdecdc7d2e2
saiPriyankaPanchadi <panchadisaipriyanka@gmail.com>
2019-12-16 17:54:58 +0530

    1 file modified

--- Homepage.js
+++ Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import {Register} from 'react-router-dom'
 import Home from './Home.css';
 
 const Homepage = () => <div className='Home'>Homepage
 <div><Link to="/login" >Login </Link></div>
+<div><Link to="/register" >Register </Link></div>
 </div>
 
 export default Homepage
